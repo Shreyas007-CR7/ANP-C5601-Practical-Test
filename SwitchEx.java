bf972d1bcd223735772d3eaeae2ac47a2151e6ac
Shreyas Joshi <shreyasjoshi964@gmail.com>
2023-09-20 12:47:34 +0530

    First Commit

---
+++ SwitchEx.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class SwitchEx {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String[] names=new String[10];
+		int[]marks = new int[10];
+		char[]grades=new char[10];
+		
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("Enter name of Student:"+(i+1));
+			names[i]=sc.next();
+			System.out.println("Enter the marks of:"+(i+1));
+			marks[i]=sc.nextInt();
+			
+			
+			switch(marks[i]/10)
+			{
+			case 10:
+			case 9:	
+				grades[i]='A';
+				break;
+			
+			case 8:
+				grades[i]='B';
+				break;
+			case 7:
+				grades[i]='C';
+				break;
+			case 6:
+				grades[i]='D';
+				break;
+			case 5:
+				grades[i]='E';
+				break;
+			default:
+				grades[i]='F';
+				
+				
+			}
+		}
+		System.out.println("\nStudent Details");
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("\nName of Student ="+names[i]+"\nMarks of Students:"+marks[i]+"\n Grade of Student:"+grades[i]);
+		}
+	}
+
+}
